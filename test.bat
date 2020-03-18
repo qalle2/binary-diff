e1e7da2c6ed67a01193380fe232035b16828869e
Kalle <qalle85@gmail.com>
2020-03-18 19:39:58 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,13 +1,13 @@
 @echo off
 cls
 
-echo === test.bat: a, b (quiet) ===
-python binary_diff.py --quiet test\a.txt test\b.txt
-echo.
-
 echo === test.bat: a, b ===
 python binary_diff.py test\a.txt test\b.txt
 echo.
 
 echo === test.bat: smb ===
-python binary_diff.py --minimum-match-length 1024 test\smb-w.prg test\smb-e.prg
+python binary_diff.py --min-match-len 1024 test\smb-w.prg test\smb-e.prg
+echo.
+
+echo === test.bat: help ===
+python binary_diff.py --help
